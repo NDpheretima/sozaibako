03a14b5229a1df7b91ebc8b511520aafdccbe582
EC2 Default User <ec2-user@ip-172-31-8-247.ec2.internal>
2023-01-27 16:34:46 +0000

    マイページの作成/プロフィール編集機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
-  resources :users, only: [:show, :edit]
-  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :edit, :update]
+  resources :artworks, only: [:index, :show, :create, :edit, :update, :destroy]
 end
